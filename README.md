81706afc3cf7899bfdfe7d38345be8fc6ca8f12a
Ogwuegbu Maxwell <53094485+OgwuegbuMaxwell@users.noreply.github.com>
2024-11-21 06:04:31 -0600

    homepage react

---
+++ README.md
@@ -0,0 +1,34 @@
+## Stock Oracle
+
+Stock Oracle is a web application designed to predict stock prices using machine learning, specifically integrating Keras and LSTM models. Built with Django, Django REST Framework, and React, it provides insightful forecasts based on 100-day and 200-day moving averages, key indicators used by stock analysts to guide investment decisions.
+
+### Features
+
+ - Machine Learning Predictions: Utilizes LSTM models for accurate stock price forecasting.
+
+ - Moving Average Analysis: Leverages 100-day and 200-day moving averages to provide valuable insights.
+
+ - Modern Tech Stack: Built using Django for backend, Django REST Framework for APIs, and React for the frontend.
+
+
+## Technologies Used
+
+ - Backend: Django, Django REST Framework
+
+ - Frontend: React, JavaScript
+
+ - Machine Learning: Keras, LSTM model
+
+
+
+## Screenshots
+
+
+### Home Page
+Homepage of the app.
+![Screenshot (288)](https://github.com/user-attachments/assets/f006e2a3-12a6-440a-9a59-54389b083bfe)
+
+
+
+
+
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className="p-5 text-center bg-light-dark rounded">
+                <h1 className='text-light'>Stock Prediction App</h1>
+                <h4 className='text-light'>Welcome to Stock Oracle, your gateway to intelligent stock price forecasting.</h4>
+                <p className='text-light lead'>
+                    This application utilizes cutting-edge machine learning techniques, specifically employing Keras with an LSTM model, integrated seamlessly within the Django framework. By analyzing crucial market data, including 100-day and 200-day moving averages, Stock Oracle delivers powerful insights into future stock price movements. These indicators are key metrics used by financial analysts worldwide to support informed trading and investment decisions.
+                </p>
+                <Button text="Login" class="btn-info"/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
